bff25c9a7f034040dc3c1c76f3a085f15464e541
Ian Stolte <stolte@usc.edu>
2025-04-18 00:32:34 +0000

    Finished HW 5!

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,10 +20,15 @@ using namespace std;
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
-// Add prototypes for any helper functions here
+bool scheduleHelper(
+    AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    vector<size_t>& currShifts,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    int start
+);
 
-
-// Add your implementation of schedule() and other helper functions here
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -36,10 +41,57 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
 
+    vector<unsigned int> currRow;
+    sched.push_back(currRow);
+    vector<size_t> currShifts;
+    for (size_t i = 0; i < avail[0].size(); i++)
+    {
+        currShifts.push_back(0);
+    }
+    AvailabilityMatrix availEditable = avail;
+    return scheduleHelper(availEditable, dailyNeed, currShifts, maxShifts, sched, 0);
+}
 
+bool scheduleHelper(
+    AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    vector<size_t>& currShifts,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    int start
+)
+{
+    if (sched.back().size() == dailyNeed)
+    {
+        if (sched.size() == avail.size())
+            return true;
+        vector<unsigned int> currRow;
+        sched.push_back(currRow);
+        start = 0;
+    }
 
+    for (size_t i = start; i < avail[0].size(); i++)
+    {
+        if (avail[sched.size()-1][i] == false || currShifts[i] == maxShifts)
+            continue;
 
+        sched[sched.size()-1].push_back(i);
+        avail[sched.size()-1][i] = false;
+        currShifts[i]++;
+        if (scheduleHelper(avail, dailyNeed, currShifts, maxShifts, sched, i+1))
+        {
+            return true;
+        }
+        else
+        {
+            sched[sched.size()-1].pop_back();
+            avail[sched.size()-1][i] = true;
+            currShifts[i]--;
+        }
+    }
+    if (sched[sched.size()-1].size() == 0)
+        sched.pop_back();
+    return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -4,6 +4,7 @@
 // For std::remove
 #include <algorithm> 
 #include <map>
+#include <vector>
 #include <set>
 #endif
 
@@ -12,17 +13,63 @@
 using namespace std;
 
 
-// Add prototypes of helper functions here
+void wordleHelper(size_t n, string temp, multiset<char>& floating,
+  const set<string>& dict, set<string>& results);
 
 
 // Definition of primary wordle function
-std::set<std::string> wordle(
-    const std::string& in,
-    const std::string& floating,
-    const std::set<std::string>& dict)
+set<string> wordle(
+  const string& in,
+  const string& floating,
+  const set<string>& dict)
 {
-    // Add your code here
-
+    set<string> results;
+    multiset<char> floatingSet;
+    for (size_t i = 0; i < floating.length(); i++)
+    {
+      floatingSet.insert(floating[i]);
+    }
+    string temp = in;
+    wordleHelper(0, temp, floatingSet, dict, results);
+    return results;
 }
 
-// Define any helper functions here
+void wordleHelper(size_t n, string temp, multiset<char>& floating,
+  const set<string>& dict, set<string>& results)
+{
+    if (n > temp.length()) //word is complete
+    {
+      if (floating.size() == 0 && dict.find(temp) != dict.end())
+        results.insert(temp);
+
+      return;
+    }
+    else if (temp.length()-n < floating.size()) //not enough space to use up floating chars
+    {
+      return;
+    }
+    
+    if (temp[n] != '-') //if given char, just move to next spot
+    {
+      wordleHelper(n+1, temp, floating, dict, results);
+    }
+    else
+    {
+      for (char c = 'a'; c <= 'z'; c++)
+      {
+        temp[n] = c;
+
+        bool erased = false;
+        multiset<char>::iterator it = floating.find(c);
+        if (it != floating.end())
+        {
+          floating.erase(it);
+          erased = true;
+        }
+        wordleHelper(n+1, temp, floating, dict, results);
+
+        if (erased)
+          floating.insert(c);
+      }
+    }
+}
